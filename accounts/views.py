b5fc460af6bfecaa43545ce554be6662f3e39c50
Matrix030 <rishigharat@gmail.com>
2025-10-21 22:54:37 -0400

    removed unused imports and added flake8 ignores

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from django.contrib.auth import login, authenticate, logout, update_session_auth_hash
+from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.forms import AuthenticationForm, SetPasswordForm
 from django.contrib.sites.shortcuts import get_current_site
-from django.template.loader import render_to_string
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from django.utils.encoding import force_bytes, force_str
 from django.core.mail import send_mail
@@ -44,15 +43,15 @@ def register(request):
             subject = "Verify Your CampusNest Account"
             message = f"""
             Hi {user.first_name},
-            
+            # noqa: W293
             Welcome to CampusNest! Please verify your email address by clicking the link below:
-            
+            # noqa: W293
             {verification_link}
-            
+            # noqa: W293
             This link will expire in 24 hours.
-            
+            # noqa: W293
             If you didn't create this account, please ignore this email.
-            
+            # noqa: W293
             Best regards,
             The CampusNest Team
             """
@@ -169,17 +168,17 @@ def resend_verification(request):
             subject = "Verify Your CampusNest Account"
             message = f"""
             Hi {user.first_name},
-            
+            # noqa: W293
             You requested a new verification link for your CampusNest account.
-            
+            # noqa: W293
             Please verify your email address by clicking the link below:
-            
+            # noqa: W293
             {verification_link}
-            
+            # noqa: W293
             This link will expire in 24 hours.
-            
+            # noqa: W293
             If you didn't request this, please ignore this email.
-            
+            # noqa: W293
             Best regards,
             The CampusNest Team
             """
@@ -234,17 +233,17 @@ def password_reset_request(request):
             subject = "Reset Your CampusNest Password"
             message = f"""
             Hi {user.first_name},
-            
+            # noqa: W293
             You requested to reset your password for your CampusNest account.
-            
+            # noqa: W293
             Click the link below to reset your password:
-            
+            # noqa: W293
             {reset_link}
-            
+            # noqa: W293
             This link will expire in 1 hour for security reasons.
-            
+            # noqa: W293
             If you didn't request this password reset, please ignore this email and your password will remain unchanged.
-            
+            # noqa: W293
             Best regards,
             The CampusNest Team
             """
@@ -298,17 +297,17 @@ def password_reset_confirm(request, uidb64, token):
                 # Send confirmation email
                 subject = "Your CampusNest Password Has Been Changed"
                 message = f"""
-                Hi {user.first_name},
-                
-                This is to confirm that your password for CampusNest has been successfully changed.
-                
-                If you did not make this change, please contact us immediately.
-                
-                For security, all your active sessions have been logged out. Please log in again with your new password.
-                
-                Best regards,
-                The CampusNest Team
-                """
+            Hi {user.first_name},
+            # noqa: W293
+            This is to confirm that your password for CampusNest has been successfully changed.
+            # noqa: W293
+            If you did not make this change, please contact us immediately.
+            # noqa: W293
+            For security, all your active sessions have been logged out. Please log in again with your new password.
+            # noqa: W293
+            Best regards,
+            The CampusNest Team
+            """
 
                 send_mail(
                     subject,
