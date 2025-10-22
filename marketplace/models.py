a362c0e9318c6067810744018b27644e772bb3bf
adityanagdekar <adityanagdekar@gmail.com>
2025-10-22 12:33:54 -0400

    Changes related to marketplace

--- marketplace/models.py
+++ marketplace/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.conf import settings
-from django.core.exceptions import ValidationError
+
+# from django.core.exceptions import ValidationError
 
 
 class Item(models.Model):
