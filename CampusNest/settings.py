f783abbd05892db652b890f55a22bc090214fbaa
Matrix030 <rishigharat@gmail.com>
2025-10-25 19:51:51 -0400

    updated databse

--- CampusNest/settings.py
+++ CampusNest/settings.py
@@ -118,12 +118,12 @@ else:
     }
 
 # Uncomment while testing so that RDS does not get used when testing
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
